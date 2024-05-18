e1523dea814d7d23341d2f915cb846cac24cf96c
ashab-k <ashabkhann2000@gmail.com>
2024-05-18 11:15:30 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="mx-[10vw]">
+      <div>Hello world</div>
+    </main>
+  );
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+export default function Navbar() {
+  return (
+    <nav>
+      <Image />
+    </nav>
+  );
+}
